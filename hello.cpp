27e8def3deb3424b9618361574d657df599dc2e0
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2024-12-04 10:35:03 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int main() {
-
+    int x;
+    cin >> x;
+    cout << "value of x: " << x << "\n";
     cout << "I'm BATMAN" << "\n" << "Buzzinga" << "\n" << "i write code, love math";
     return 0;
 }
