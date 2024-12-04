c60f224137ae369d8f0fa7f18c67eef39a1de585
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2024-12-04 15:55:31 +0530

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // int arr[5];
+    // cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+
+    // arr[3] += 10;
+    // cout << arr[3];
+
+    // 2D array
+    int arr [3][5];
+
+    arr[1][3] = 48;
+    cout << arr[1][3];
+
+    return 0;
+}
