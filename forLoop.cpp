8281e2ceace179962cabdd2a581453d4c87ba9d9
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2024-12-04 16:15:23 +0530

    Create forLoop.cpp

---
+++ forLoop.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    // for(int i=1; i<=10; i = i + 1){
+    //     cout << "Batman" << endl;
+    // }
+
+    // for(int i = 1; i <= 25; i = i + 3){
+    //     cout << "Batman" << i << endl;
+    // }
+
+    for(int i = 5; i >= 0; i = i - 1){
+        cout << "Batman" << i << endl;
+    }
+
+    return 0;
+}
