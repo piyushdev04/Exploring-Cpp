c24be7723e5256d5e8a2a2c91c0958c3a4cb8ce4
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2024-12-04 16:25:34 +0530

    Create whileLoop.cpp

---
+++ whileLoop.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    // int i = 1;
+    // while(i <= 5){
+    //     cout << "Batman " << i << endl;
+    //     i = i + 1;
+    // }
+
+    int i = 2;
+    do {
+        cout << "Batman " << i << endl;
+        i = i + 1;
+    } while (i <= 1);
+    cout << i;
+
+    return 0;
+}
