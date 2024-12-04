9138c1b1e4cab66a2f3f6b6b6c5d3b155aaf7531
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2024-12-04 10:38:49 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,10 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int x;
-    cin >> x;
-    cout << "value of x: " << x << "\n";
+    int x, y;
+    cin >> x >> y;
+    cout << "value of x: " << x << " and y: " << y << "\n";
     cout << "I'm BATMAN" << "\n" << "Buzzinga" << "\n" << "i write code, love math";
     return 0;
 }
